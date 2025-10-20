22f46e77630c52452979936932b70d4b41233856
Arthur Rodrigues Andrade <70665984+Arthur225@users.noreply.github.com>
2025-10-20 12:50:42 -0300

    Refactor coinChange function to use dynamic programming with improved logic for minimum coin calculation

--- main.cpp
+++ main.cpp
@@ -84,15 +84,20 @@ void printChange(const vector<int>& change) {
     cout << "Total coins given: " << change.size() << endl;
 }
 
-int coinChange(vector<int>& coins, int amount) {
-        vector<long>minimumCoins(amount+1, INT_MAX);
-        minimumCoins[0] = 0;
-        for(auto coin: coins){
-            for(int j = coin; j<=amount; j++){
-                if(minimumCoins[j - coin] != INT_MAX) minimumCoins[j] = min(minimumCoins[j], 1 + minimumCoins[j - coin]);
-            }
+int coinChange(vector<int>& coins, int n) {
+    int dp[++n];
+    dp[0] = 0;
+
+    sort(begin(coins), end(coins));
+
+    for (int i = 1; i < n; i++) {
+        dp[i] = INT_MAX;
+        for (int c: coins) {
+            if (i - c < 0) break;
+            if (dp[i - c] != INT_MAX) dp[i] = min(dp[i], 1 + dp[i - c]);
         }
-        return minimumCoins[amount] == INT_MAX ? -1 : minimumCoins[amount];
+    }
+    return dp[--n] == INT_MAX ? -1 : dp[n];
 }
 
 int main() {
